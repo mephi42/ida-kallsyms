83f4bfddb081a0d7945a3569f9ff199203adfb2a
mephi42 <mephi42@gmail.com>
2019-10-10 18:55:15 +0200

    Add 3.10.0-862.11.6.el7.x86_64 test

--- test/test_find_kallsyms.py
+++ test/test_find_kallsyms.py
@@ -21,6 +21,12 @@ class TestFindKallsyms(unittest.TestCase):
         self.assertEquals('T_text', addresses_and_names[0][1])
         self.assertEquals('B__bss_stop', addresses_and_names[-1][1])
 
+    def test_kallsyms_3_10_0_x86_64(self):
+        addresses_and_names = list(find_kallsyms_in_rodata(
+            self._read('kallsyms-3.10.0-862.11.6.el7.x86_64.gz'), '<'))
+        self.assertEquals('Airq_stack_union', addresses_and_names[0][1])
+        self.assertEquals('B__brk_limit', addresses_and_names[-1][1])
+
 
 if __name__ == '__main__':
     unittest.main()
