e34a93a2d3edd5c1574bdd0de10d526e16bc9068
mephi42 <mephi42@gmail.com>
2019-10-10 18:28:53 +0200

    Add 4.16.3-301.fc28.s390x test

---
+++ test/test_find_kallsyms.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import gzip
+import os
+import unittest
+
+from find_kallsyms import find_kallsyms_in_rodata
+
+
+class TestFindKallsyms(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        unittest.TestCase.__init__(self, *args, **kwargs)
+        self.basedir = os.path.dirname(__file__)
+
+    def _read(self, name):
+        with gzip.GzipFile(os.path.join(self.basedir, name)) as fp:
+            return bytearray(fp.read())
+
+    def test_kallsyms_4_16_3_s390x(self):
+        addresses_and_names = list(find_kallsyms_in_rodata(
+            self._read('kallsyms-4.16.3-301.fc28.s390x.gz'), '>'))
+        self.assertEquals('T_text', addresses_and_names[0][1])
+        self.assertEquals('B__bss_stop', addresses_and_names[-1][1])
+
+
+if __name__ == '__main__':
+    unittest.main()
