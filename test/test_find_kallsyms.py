5315c7ca4eb7b2e1bf02d944cadc2b49a71a10a5
mephi42 <mephi42@gmail.com>
2019-10-15 23:30:43 +0200

    Test # of symbols found

--- test/test_find_kallsyms.py
+++ test/test_find_kallsyms.py
@@ -18,24 +18,28 @@ class TestFindKallsyms(unittest.TestCase):
     def test_kallsyms_4_16_3_s390x(self):
         addresses_and_names = list(find_kallsyms_in_rodata(
             self._read('kallsyms-4.16.3-301.fc28.s390x.gz')))
+        self.assertEqual(62766, len(addresses_and_names))
         self.assertEquals('T_text', addresses_and_names[0][1])
         self.assertEquals('B__bss_stop', addresses_and_names[-1][1])
 
     def test_kallsyms_3_10_0_x86_64(self):
         addresses_and_names = list(find_kallsyms_in_rodata(
             self._read('kallsyms-3.10.0-862.11.6.el7.x86_64.gz')))
+        self.assertEqual(82619, len(addresses_and_names))
         self.assertEquals('Airq_stack_union', addresses_and_names[0][1])
         self.assertEquals('B__brk_limit', addresses_and_names[-1][1])
 
     def test_kallsyms_5_1_9_x86_64(self):
         addresses_and_names = list(find_kallsyms_in_rodata(
             self._read('kallsyms-5.1.9.balsnctl2019.krazynote.x86_64.gz')))
+        self.assertEqual(74045, len(addresses_and_names))
         self.assertEquals('Airq_stack_union', addresses_and_names[0][1])
         self.assertEquals('B__brk_limit', addresses_and_names[-1][1])
 
     def test_kallsyms_5_3_0_x86_64(self):
         addresses_and_names = list(find_kallsyms_in_rodata(
             self._read('kallsyms-5.3.0.hitcon2019.poe.x86_64.gz')))
+        self.assertEqual(88612, len(addresses_and_names))
         self.assertEquals('Afixed_percpu_data', addresses_and_names[0][1])
         self.assertEquals('T__init_scratch_end', addresses_and_names[-1][1])
 
