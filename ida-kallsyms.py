52fc957acc99c37bf2f22f1007967a3cddc8ac19
mephi42 <mephi42@gmail.com>
2020-05-18 23:05:45 +0200

    ida: handle dummy name prefixes

--- ida-kallsyms.py
+++ ida-kallsyms.py
@@ -1,5 +1,5 @@
 from idaapi import get_bytes
-from ida_name import set_name
+from ida_name import is_uname, set_name
 from ida_segment import get_segm_by_name
 
 from find_kallsyms import find_kallsyms_in_rodata
@@ -11,4 +11,8 @@ rodata_size = rodata_segm.end_ea - rodata_segm.start_ea + 1
 rodata = b''.join(get_bytes(rodata_segm.start_ea, rodata_size))
 for address, name in find_kallsyms_in_rodata(rodata):
     if name[0] != 'A':
-        set_name(address, str(name[1:]))
+        new_name = str(name[1:])
+        if not is_uname(new_name):
+            new_name = '_' + new_name
+        if is_uname(new_name):
+            set_name(address, new_name)
