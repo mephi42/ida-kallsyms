c769e05307eabc6b97cd50279b33fa33c34e128a
vient <lozko.roma@gmail.com>
2022-08-19 01:57:26 +0300

    get_bytes returns bytes (#1)
    since IDA 7.6, maybe even earlier

--- ida-kallsyms.py
+++ ida-kallsyms.py
@@ -7,6 +7,6 @@ rodata_segm = get_segm_by_name('.rodata')
 if rodata_segm is None:
     rodata_segm = get_segm_by_name('.text')
 rodata_size = rodata_segm.end_ea - rodata_segm.start_ea + 1
-rodata = b''.join(get_bytes(rodata_segm.start_ea, rodata_size))
+rodata = get_bytes(rodata_segm.start_ea, rodata_size)
 kallsyms = find_kallsyms.find_kallsyms_in_rodata(rodata)  # noqa: F821
 ida_utils.apply_kallsyms(kallsyms)  # noqa: F821
