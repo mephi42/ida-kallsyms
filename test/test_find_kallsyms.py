deecd3e687b5459e9908a79acaba9a0c329686b1
mephi42 <mephi42@gmail.com>
2023-08-17 19:21:30 +0200

    Add CI

--- test/test_find_kallsyms.py
+++ test/test_find_kallsyms.py
@@ -19,21 +19,21 @@ class TestFindKallsyms(unittest.TestCase):
         addresses_and_names = list(find_kallsyms_in_rodata(
             self._read('kallsyms-4.16.3-301.fc28.s390x.gz')))
         self.assertEqual(62766, len(addresses_and_names))
-        self.assertEquals((0, 'T_text'), addresses_and_names[0])
-        self.assertEquals((0xd31e00, 'B__bss_stop'), addresses_and_names[-1])
+        self.assertEqual((0, 'T_text'), addresses_and_names[0])
+        self.assertEqual((0xd31e00, 'B__bss_stop'), addresses_and_names[-1])
 
     def test_kallsyms_3_10_0_x86_64(self):
         addresses_and_names = list(find_kallsyms_in_rodata(
             self._read('kallsyms-3.10.0-862.11.6.el7.x86_64.gz')))
         self.assertEqual(82619, len(addresses_and_names))
-        self.assertEquals((0, 'Airq_stack_union'), addresses_and_names[0])
+        self.assertEqual((0, 'Airq_stack_union'), addresses_and_names[0])
         dump_stack_address, = [
             address
             for address, name in addresses_and_names
             if name == 'Tdump_stack'
         ]
         self.assertEqual(0xffffffff817135bb, dump_stack_address)
-        self.assertEquals(
+        self.assertEqual(
             (0xffffffff82657000, 'B__brk_limit'),
             addresses_and_names[-1],
         )
@@ -42,23 +42,23 @@ class TestFindKallsyms(unittest.TestCase):
         addresses_and_names = list(find_kallsyms_in_rodata(
             self._read('kallsyms-5.1.9.balsn2019.krazynote.x86_64.gz')))
         self.assertEqual(74045, len(addresses_and_names))
-        self.assertEquals((0, 'Airq_stack_union'), addresses_and_names[0])
-        self.assertEquals(
+        self.assertEqual((0, 'Airq_stack_union'), addresses_and_names[0])
+        self.assertEqual(
             (0xffffffff82a2c000, 'B__brk_limit'), addresses_and_names[-1])
 
     def test_kallsyms_5_1_0_aarch64(self):
         addresses_and_names = list(find_kallsyms_in_rodata(
             self._read('kallsyms-5.1.0.tasteless2019.tee.aarch64.gz')))
         self.assertEqual(117079, len(addresses_and_names))
-        self.assertEquals((0, 't_head'), addresses_and_names[0])
-        self.assertEquals((0x13ce000, 'B_end'), addresses_and_names[-1])
+        self.assertEqual((0, 't_head'), addresses_and_names[0])
+        self.assertEqual((0x13ce000, 'B_end'), addresses_and_names[-1])
 
     def test_kallsyms_5_3_0_x86_64(self):
         addresses_and_names = list(find_kallsyms_in_rodata(
             self._read('kallsyms-5.3.0.hitcon2019.poe.x86_64.gz')))
         self.assertEqual(88612, len(addresses_and_names))
-        self.assertEquals((0, 'Afixed_percpu_data'), addresses_and_names[0])
-        self.assertEquals(
+        self.assertEqual((0, 'Afixed_percpu_data'), addresses_and_names[0])
+        self.assertEqual(
             (0xffffffff83200000, 'T__init_scratch_end'),
             addresses_and_names[-1],
         )
@@ -67,26 +67,26 @@ class TestFindKallsyms(unittest.TestCase):
         addresses_and_names = list(find_kallsyms_in_rodata(
             self._read('kallsyms-4.4.0-1085-raspi2.arm.gz')))
         self.assertEqual(78413, len(addresses_and_names))
-        self.assertEquals((0x80008000, 'Tstext'), addresses_and_names[0])
-        self.assertEquals((0x80f56454, 'B__bss_stop'), addresses_and_names[-1])
+        self.assertEqual((0x80008000, 'Tstext'), addresses_and_names[0])
+        self.assertEqual((0x80f56454, 'B__bss_stop'), addresses_and_names[-1])
 
     def test_kallsyms_4_4_223_i686(self):
         addresses_and_names = list(find_kallsyms_in_rodata(
             self._read('kallsyms-4.4.223.defcon2020.ooofs.i686.gz')))
         self.assertEqual(80397, len(addresses_and_names))
-        self.assertEquals((0xc1000000, 'Tstartup_32'), addresses_and_names[0])
-        self.assertEquals(
+        self.assertEqual((0xc1000000, 'Tstartup_32'), addresses_and_names[0])
+        self.assertEqual(
             (0xc1e9B000, 'B__brk_limit'), addresses_and_names[-1])
 
     def test_kallsyms_4_4_223_i686_v2(self):
         addresses_and_names = list(find_kallsyms_in_rodata(
             self._read('kallsyms-4.4.223.defconfig.i686.gz')))
         self.assertEqual(39874, len(addresses_and_names))
-        self.assertEquals(
+        self.assertEqual(
             (0xc1000338, 'tsanitize_boot_params.constprop.0'),
             addresses_and_names[0],
         )
-        self.assertEquals((0xc1be29bd, 'T_einittext'), addresses_and_names[-1])
+        self.assertEqual((0xc1be29bd, 'T_einittext'), addresses_and_names[-1])
 
 
 if __name__ == '__main__':
